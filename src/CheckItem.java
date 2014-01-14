7343de0c2b54075dfe83fe91371bb7a6cd68d174
andrytret <andrytret91@gmail.com>
2014-01-14 13:07:18 +0300

    Revert "format code, organize imports"
    This reverts commit a5ff1efff3ee90937c128824ef4bd00a69801061.

--- src/CheckItem.java
+++ src/CheckItem.java
@@ -3,7 +3,7 @@ public class CheckItem {
 	public static String checkKeys(final String keyName, final String[] keyNames) {
 
 		String result = null;
-
+		
 		if (keyNames != null && keyNames.length > 0 && keyName != null) {
 			for (int i = 0; i < keyNames.length; i++) {
 				if (keyName == keyNames[i]) {
@@ -18,43 +18,65 @@ public class CheckItem {
 		return result;
 	}
 }
+	
+
+/*public static boolean isStringInArray (final String value, final String[] array) {
+
+		boolean result = false;
+		if (array != null && array.length > 0 && value != null) {
+			for (int i = 0; i < array.length; i++) {
+				if (value.equals(array[i])) {
+					result = true;
+					break;
+				}
+			}
+		}
+		return result;
+	}
+
+}
 
 /*
- * public static boolean isStringInArray (final String value, final String[]
- * array) { boolean result = false; if (array != null && array.length > 0 &&
- * value != null) { for (int i = 0; i < array.length; i++) { if
- * (value.equals(array[i])) { result = true; break; } } } return result; } } /*
  * public static String check2 (String item1, String items[]){ String znach1 =
  * null; if (item1 == items[0]) {znach1 = items[0];} else { if (item1 ==
  * items[1]) {znach1 = items[1];} else { if (item1 == items[2]) {znach1 =
  * items[2];} else { if (item1 == items[3]) {znach1 = items[3];} else { if
  * (item1 == items[4]) {znach1 = items[4];} else { if (item1 == items[5])
  * {znach1 = items[5];} else { if (item1 == items[6]) {znach1 = items[6];} else
- * { if (item1 == items[7]) {znach1 = items[7];} else { };};};};};};};}; return
- * znach1;} } /*if (item1 == items[0]) {znach1 = items[0];} else { if (item1 ==
- * items[1]) {znach1 = items[1];} else { if (item1 == items[2]) {znach1 =
- * items[2];} else { if (item1 == items[3]) {znach1 = items[3];} else { if
- * (item1 == items[4]) {znach1 = items[4];} else { if (item1 == items[5])
- * {znach1 = items[5];} else { if (item1 == items[6]) {znach1 = items[6];} else
- * { if (item1 == items[7]) {znach1 = items[7];} else { };};};};};};};}; if
- * (item2 == items[0]) {znach2 = items[0];} else { if (item2 == items[1])
- * {znach2 = items[1];} else { if (item2 == items[2]) {znach2 = items[2];} else
- * { if (item2 == items[3]) {znach2 = items[3];} else { if (item2 == items[4])
- * {znach2 = items[4];} else { if (item2 == items[5]) {znach2 = items[5];} else
- * { if (item2 == items[6]) {znach2 = items[6];} else { if (item2 == items[7])
- * {znach2 = items[7];} else { };};};};};};};}; if (item3 == items[0]) {znach3 =
- * items[0];} else { if (item3 == items[1]) {znach3 = items[1];} else { if
- * (item3 == items[2]) {znach3 = items[2];} else { if (item3 == items[3])
- * {znach3 = items[3];} else { if (item3 == items[4]) {znach3 = items[4];} else
- * { if (item3 == items[5]) {znach3 = items[5];} else { if (item3 == items[6])
- * {znach3 = items[6];} else { if (item3 == items[7]) {znach3 = items[7];} else
- * { };};};};};};};}; if (item4 == items[0]) {znach4 = items[0];} else { if
- * (item4 == items[1]) {znach4 = items[1];} else { if (item4 == items[2])
- * {znach4 = items[2];} else { if (item4 == items[3]) {znach4 = items[3];} else
- * { if (item4 == items[4]) {znach4 = items[4];} else { if (item4 == items[5])
- * {znach4 = items[5];} else { if (item4 == items[6]) {znach4 = items[6];} else
- * { if (item4 == items[7]) {znach4 = items[7];} else { };};};};};};};}; if
- * (item5 == items[0]) {znach5 = items[0];} else { if (item5 == items[1])
+ * { if (item1 == items[7]) {znach1 = items[7];} else { };};};};};};};};
+ * 
+ * return znach1;}
+ * 
+ * }
+ * 
+ * 
+ * /*if (item1 == items[0]) {znach1 = items[0];} else { if (item1 == items[1])
+ * {znach1 = items[1];} else { if (item1 == items[2]) {znach1 = items[2];} else
+ * { if (item1 == items[3]) {znach1 = items[3];} else { if (item1 == items[4])
+ * {znach1 = items[4];} else { if (item1 == items[5]) {znach1 = items[5];} else
+ * { if (item1 == items[6]) {znach1 = items[6];} else { if (item1 == items[7])
+ * {znach1 = items[7];} else { };};};};};};};}; if (item2 == items[0]) {znach2 =
+ * items[0];} else { if (item2 == items[1]) {znach2 = items[1];} else { if
+ * (item2 == items[2]) {znach2 = items[2];} else { if (item2 == items[3])
+ * {znach2 = items[3];} else { if (item2 == items[4]) {znach2 = items[4];} else
+ * { if (item2 == items[5]) {znach2 = items[5];} else { if (item2 == items[6])
+ * {znach2 = items[6];} else { if (item2 == items[7]) {znach2 = items[7];} else
+ * { };};};};};};};};
+ * 
+ * if (item3 == items[0]) {znach3 = items[0];} else { if (item3 == items[1])
+ * {znach3 = items[1];} else { if (item3 == items[2]) {znach3 = items[2];} else
+ * { if (item3 == items[3]) {znach3 = items[3];} else { if (item3 == items[4])
+ * {znach3 = items[4];} else { if (item3 == items[5]) {znach3 = items[5];} else
+ * { if (item3 == items[6]) {znach3 = items[6];} else { if (item3 == items[7])
+ * {znach3 = items[7];} else { };};};};};};};}; if (item4 == items[0]) {znach4 =
+ * items[0];} else { if (item4 == items[1]) {znach4 = items[1];} else { if
+ * (item4 == items[2]) {znach4 = items[2];} else { if (item4 == items[3])
+ * {znach4 = items[3];} else { if (item4 == items[4]) {znach4 = items[4];} else
+ * { if (item4 == items[5]) {znach4 = items[5];} else { if (item4 == items[6])
+ * {znach4 = items[6];} else { if (item4 == items[7]) {znach4 = items[7];} else
+ * { };};};};};};};};
+ * 
+ * if (item5 == items[0]) {znach5 = items[0];} else { if (item5 == items[1])
  * {znach5 = items[1];} else { if (item5 == items[2]) {znach5 = items[2];} else
  * { if (item5 == items[3]) {znach5 = items[3];} else { if (item5 == items[4])
  * {znach5 = items[4];} else { if (item5 == items[5]) {znach5 = items[5];} else
--- src/CoreOfProgram.java
+++ src/CoreOfProgram.java
@@ -2,6 +2,37 @@ import java.awt.AWTException;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Robot;
+import java.awt.TextField;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTextField;
+
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+import javax.swing.text.BadLocationException;
+import javax.swing.JComboBox;
+import javax.swing.JLayeredPane;
+import javax.swing.JTextPane;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.*;
 
 //import calc.ProgramFrame;
 
@@ -11,9 +42,10 @@ public class CoreOfProgram {
 	 * @param args
 	 */
 	public static void main1(String[] args) {
-		// TODO tut esche ne delal
+		// TODO tut esche ne delal 
 		// c camogo nach nada zadat znach iz ProgramFrame i otkrit sam fram
-		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsEnvironment env = GraphicsEnvironment
+				.getLocalGraphicsEnvironment();
 		GraphicsDevice screen = env.getDefaultScreenDevice();
 		Robot robot = null;
 		try {
